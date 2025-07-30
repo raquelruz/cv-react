fad2fadfbd4f8ab0e69d97ada231fe236ae6b8bb
Raquel Ruiz <raquelruizlopezcepero@gmail.com>
2025-07-30 12:17:26 +0200

    feat(home): add home section

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import { useState } from "react";
-import "./App.css";
+import "./App.css"
 import { Tabs } from "./assets/components/Tabs";
 import { Navigation } from "./assets/components/Navigation";
+import { Home } from "./assets/components/Home/Home";
 
 const INITIAL_TAB = Object.keys(Tabs)[0];
 
 export const App = () => {
-  const [activeTab, setActiveTab] = useState(INITIAL_TAB);
+	const [activeTab, setActiveTab] = useState(INITIAL_TAB);
+	
 	return (
 		<>
 			<div className="app-container">
-
-        <Navigation activeTab={activeTab} setActiveTab={setActiveTab} />
-      </div>
+				<Navigation activeTab={activeTab} setActiveTab={setActiveTab} />
+			
+			<section id="Home-section">
+				<Home />
+			</section>
+			</div>
 		</>
 	);
 };
