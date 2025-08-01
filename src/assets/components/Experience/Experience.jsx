f0c7f2a105b6ded61d405889628635848e45369b
Raquel Ruiz <raquelruizlopezcepero@gmail.com>
2025-08-01 11:42:35 +0200

    feat(experience): add new styles for Experience component

--- src/assets/components/Experience/Experience.jsx
+++ src/assets/components/Experience/Experience.jsx
@@ -1,32 +1,27 @@
-import "./Experience.css"
+import "./Experience.css";
 
 export const Experience = ({ data }) => {
-    if (!data || data.length === 0) {
-        return <p>No hay experiencia disponible.</p>
-    }
+	if (!data || data.length === 0) {
+		return <p>No hay experiencia disponible.</p>;
+	}
 
-    return (
-        <section className="experience-section">
+	return (
+		<section className="experience-section">
 			<h2 className="section-title">Experiencia Profesional</h2>
-			<div className="timeline">
-				{data.map((item, index) => (
-					<div key={index} className="timeline-item">
-						<div className="timeline-dot" />
-						<div className="timeline-content">
-							<h3>{item.position}</h3>
-							<h4>{item.company}</h4>
-							<p className="experience-period">
-								{item.period} | {item.location}
-							</p>
-							<ul className="experience-description">
-								{item.description?.split("•").map((point, index) =>
-									point.trim() !== "" ? <li key={index}>{point.trim()}</li> : null
-								)}
-							</ul>
-						</div>
-					</div>
-				))}
-			</div>
+			{data.map((item, index) => (
+				<div key={index} className="experience-card">
+					<h3>{item.position}</h3>
+					<h4>{item.company}</h4>
+					<p className="experience-period">
+						{item.period} | {item.location}
+					</p>
+					<ul className="experience-description">
+						{item.description
+							?.split("•")
+							.map((point, index) => (point.trim() !== "" ? <li key={index}>{point.trim()}</li> : null))}
+					</ul>
+				</div>
+			))}
 		</section>
-    )
-}
+	);
+};
